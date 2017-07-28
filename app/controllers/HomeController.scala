34eb9057ba7f046028ba40aaeb3a8b9057837812
Chris Toomey <chris@ctoomey.com>
2017-07-28 10:28:01 -0400

    Display a form!!!

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,24 +1,36 @@
 package controllers
 
+import java.util.Date
 import javax.inject._
 import play.api._
 import play.api.mvc._
 
-/**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
-@Singleton
-class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+import play.api.data._
+import play.api.data.Forms._
 
-  /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
+case class WeatherQuery(
+  // stations: List[Int],
+  resolution: String,
+  startDate: Date,
+  endDate: Date,
+)
+
+@Singleton
+class HomeController @Inject()(cc: ControllerComponents)
+  extends AbstractController(cc) with play.api.i18n.I18nSupport {
+
+  val weatherQueryForm = Form(
+    mapping(
+      // "stations" -> list(number),
+      "resolution" -> text,
+      "startDate" -> date,
+      "endDate" -> date,
+    )(WeatherQuery.apply)(WeatherQuery.unapply)
+  )
+
   def index() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index())
+    Ok(views.html.index(weatherQueryForm))
   }
+
+  def post() = TODO
 }
